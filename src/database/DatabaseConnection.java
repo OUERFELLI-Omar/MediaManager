7465c74a1c4a4bda097e7e8c1a390cb279cb5fbc
OUERFELLI Omar <omarouerfelli10@gmail.com>
2025-05-17 16:52:06 +0100

    Done

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -12,14 +12,4 @@ public class DatabaseConnection {
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
     }
-
-    public static void closeConnection(Connection connection) {
-        if (connection != null) {
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                System.err.println("Error closing connection: " + e.getMessage());
-            }
-        }
-    }
 }
